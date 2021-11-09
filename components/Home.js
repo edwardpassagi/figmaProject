4c0041408f84497eb2d41f9dd9e136b793698dac
edwardpassagi <edward.passagi@gmail.com>
2021-11-08 21:57:57 -0600

    initialize navigation and tabbings

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native'
+
+const Home = ({navigation}) => {
+    return (
+        <View>
+            <Text>Home</Text>
+            <Button onPress={() => navigation.navigate('Details')} title="Click me"/>
+        </View>
+    );
+};
+
+export default Home;
